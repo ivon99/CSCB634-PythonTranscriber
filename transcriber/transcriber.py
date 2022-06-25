2879dfce5abe7f299f8f6a26fd749096c309a214
Peter Kostov <pekostov@gmail.com>
2022-06-25 07:12:26 +0300

    Add transcriber and testing main script

--- transcriber/transcriber.py
+++ transcriber/transcriber.py
@@ -6,18 +6,21 @@ from transcriber.transcriber_config import TranscriberConfig
 
 class Transcriber(object):
     def __init__(self, config: TranscriberConfig) -> None:
-        self.model = deepspeech.Model(config.model_path, config.beam_width)
-        self.model.enableDecoderWithLM(config.lm_file_path, config.trie_file_path, config.lm_alpha, config.lm_beta)
+        self.model = deepspeech.Model(config.model_path)
+        self.model.setBeamWidth(config.beam_width)
+        self.model.enableExternalScorer(config.scorer_path)
+        self.model.setScorerAlphaBeta(config.lm_alpha, config.lm_beta)
     
     def create_stream(self) -> None:
         self.context = self.model.createStream()
 
     def finish_stream(self) -> str:
-        return self.model.finishStream(self.context)
+        return self.context.finishStream()
+
+    def debug(self):
+        text = self.context.intermediateDecode()
+        print('DEBUG: {}'.format(text))
 
     def transcribe(self, data) -> None:
         data16 = np.frombuffer(data, dtype=np.int16)
-        self.model.feedAudioContent(self.context, data16)
-        # Debug only
-        # text = self.model.intermediateDecode(context)
-        # print('Interim text = {}'.format(text))
+        self.context.feedAudioContent(data16)
