4a9700a43ca5fccf6703e7d3009611bc84bf1ed1
ivon99 <ivon.tsonkova@gmail.com>
2022-07-02 10:16:29 +0300

    Add docstring for audiostreamer and filewriter

--- audiostreamer/audiostreamer.py
+++ audiostreamer/audiostreamer.py
@@ -1,11 +1,28 @@
+"""Module for recording an audio stream from microphone.
+
+The module has one class- Audio Streamer which uses PyAudio library for creating and manipulating an audio stream.
+"""
 import pyaudio 
+
 from transcriber import *
  
 class AudioStreamer(object):
+	"""Class for creating and manipulating an audio stream.
+	
+	The class uses PyAudio library to create, manipulate and close an audio stream.
+	
+	Attributes:
+		_audio: Object instance for class PuAudio providing an interface for PortAudio.
+		_stream: Object instace for PyAudio class Stream
+		channels: An integer showing number of channels. (1 for mono, 2 for stereo)
+		rate: An integer showing the microphone sampling rate in Hertz. 
+		input: Boolean value specifying whether the stream is an input one. 
+		frames_per_buffer: An integer specifying the number of frames per buffer.
+	"""
 	def __init__(self, format, channels, rate, input, frames_per_buffer) -> None:
+		"""Initializes AudioStreamer object with given parameters"""
 		self._audio = pyaudio.PyAudio()
 		self._stream = None
-		self.frames = []
 		self.format= format
 		self.channels=channels
 		self.rate= rate
@@ -13,14 +30,17 @@ class AudioStreamer(object):
 		self.frames_per_buffer=frames_per_buffer
 	
 	def start_streaming(self) -> None:
+		"""Starts the audio stream"""
 		self._stream.start_stream()
 		
 	def finish_recording(self) -> None:
+		"""Stops and terminates the audio stream and closes the session"""
 		self._stream.stop_stream()
 		self._stream.close()
 		self._audio.terminate()
 	
 	def record(self, transcriber) -> None:
+		"""Starts recording audio input"""
 		self._stream = self._audio.open(
 			format=self.format,
 			channels= self.channels,
@@ -31,10 +51,12 @@ class AudioStreamer(object):
 		self.start_streaming()
 		 
 	def audio_processing(self, transcriber):
+		"""Feeds audio stream data to transcriber"""
 		def callback(in_data, frame_count, time_info, status):
 			transcriber.transcribe(in_data)
 			return (in_data, pyaudio.paContinue)
 		return callback
 	
 	def is_active_stream(self) -> bool:
+		"""Returns whether the audio stream is active"""
 		return self._stream.is_active()
--- filewriter/filewriter.py
+++ filewriter/filewriter.py
@@ -1,11 +1,25 @@
+"""Module for saving transcriptions to text files.
+
+The module has one class - FileWriter.
+"""
 from datetime import datetime 
 
 class FileWriter(object):
+	"""Class for saving input text to text files.
+	
+	The class can save text to files with automatically generated filenames from date and time of creation or with inputted from user filenames.
+	
+	Attributes:
+		text: A string of text to be saved in file.
+		filepath: A string of filepath to directory where to save the text file.
+	"""
 	def __init__(self, text_to_save, filepath) -> None:
+		"""Initializes FileWriter object with given text to save and filepath to where to save"""
 		self.text= text_to_save
 		self.filepath= filepath
 	
 	def save_text_automatic_filename(self) -> str:
+		"""Saves text to file with automatically generated filename from current date and time"""
 		curr_time = datetime.now()
 		curr_time_str = curr_time.strftime("%y-%m-%d_%H:%M:%S")
 		filename= self.filepath +'/'+ curr_time_str
@@ -15,6 +29,7 @@ class FileWriter(object):
 		return filename
 	
 	def save_text_inputted_filename(self, in_filename) -> None:
+		"""Saves text to file with given filename"""
 		filename= self.filepath+ '/' + in_filename
 		f = open(filename,'a')
 		f.write(" ")
